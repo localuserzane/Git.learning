e08b329a19fdbb5ce47145fc691c9bdb97357c51
localuserzane <zanen@inbox.lv>
2019-07-08 10:37:11 +0300

    bla bla

--- src/main/java/com/example/demo/repo/CarRepo.java
+++ src/main/java/com/example/demo/repo/CarRepo.java
@@ -11,7 +11,7 @@ public interface CarRepo extends CrudRepository<Car, Integer>{
 	ArrayList<Car> findByYear(int year);
 	ArrayList<Car> findByYearAndSpeed(int year, int speed);
 	/* @Query(value="SELECT...")
-	Car myquery(); komentars turpinas un nekad nebeidzas nekad
+	Car myquery(); komentars turpinas un nekad nebeidzas nekad nekad nekad
 */
 	
 }
